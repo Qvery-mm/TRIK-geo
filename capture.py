610a918b7d3fe0a6be14e49d5047978bd779603b
Aleksandr Kovalchukov <aleksandr.kovalchukov@mail.ru>
2023-12-21 14:42:44 +0300

    demo

---
+++ capture.py
@@ -0,0 +1,17 @@
+import cv2
+import panorama
+
+camera = cv2.VideoCapture(0)
+ret, pano = camera.read()
+for i in range(200):
+    print(i)
+    try:
+        ret, frame = camera.read()
+        new = panorama.makePano(pano, frame)
+        cv2.imshow("out", new)
+        pano = new
+    except Exception as e:
+        print(e)
+        print("Skipping frame")
+    cv2.waitKey(1)
+del(camera)
